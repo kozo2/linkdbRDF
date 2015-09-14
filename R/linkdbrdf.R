41a2a8eefa8a1c73d688a5139365bf558ada4d2e
Kozo Nishida <knishida@riken.jp>
2015-09-14 22:47:04 +0900

    fix bugs

--- R/linkdbrdf.R
+++ R/linkdbrdf.R
@@ -13,17 +13,20 @@ getCpdLinks <- function(fromdb, todb, inference = FALSE, limit = 0, endpoint="ht
   sparql_base <- paste( "BASE <http://www.genome.jp/linkdb/> \n",
                         "SELECT ?fromlabel ?tolabel WHERE { \n",
                         "?from <equivalent> ?to . \n",
+                        
                         "?from <core/database> ?fromdb . \n",
                         "?to <core/database> ?todb . \n",
-                        '?fromdb <core/dblabel> "', fromdb, '". \n',
-                        '?todb <core/database> "', todb, '" . \n',
+                        
+                        "?fromdb <core/dblabel> \"", fromdb, "\" . \n",
+                        "?todb <core/dblabel> \"", todb, "\" . \n",
+                        
                         "?from rdfs:label ?fromlabel . \n",
                         "?to   rdfs:label ?tolabel . \n",
                         "} ORDER BY ?fromlabel ?tolabel \n",
-                 limitC )
+                 limitC , sep="")
   print (sparql_base)
   if(inference) {
-    query <- paste( "DEFINE input:inference 'http://reactionontology.org/inference' \n", sparql_base)
+    query <- paste( "DEFINE input:inference 'http://www.genome.jp/sparql/linkdb/' \n", sparql_base)
   }
   else {
     query <- sparql_base
@@ -35,7 +38,7 @@ getCpdLinks <- function(fromdb, todb, inference = FALSE, limit = 0, endpoint="ht
               SPARQL(url=endpoint,query)
           },
           error = function(err){
-              message("an error occured when trying to query for ensembl genes ", err)
+              message("an error occured when trying to query for linkdb ", err)
           })#end tryCatch
 
   #res<-SPARQL(url=endpoint,query)
